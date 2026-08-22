9e2b57abb768e00460458063a6b5ecbfd883dcc1
ba <ba>
2026-08-22 19:30:06 +0200

    Add tests

--- Parser.Tests/UnitTest.cs
+++ Parser.Tests/UnitTest.cs
@@ -38,13 +38,13 @@ public class ParserApiTests
     }
     
     [Test]
-    public async Task ShouldParseCsvInvalidBase64()
+    public async Task InvalidBase64()
     {
         var request = new RestRequest("/api/v1/parse-content", Method.Post);
         request.AddJsonBody(new
         {
             Type = "csv",
-            Content = "abc"
+            Content = "InvalidBase64"
         });
 
         RestResponse response = await _restClient.ExecuteAsync(request);
@@ -54,6 +54,61 @@ public class ParserApiTests
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
         Assert.That(detail, Is.EqualTo("Invalid Base64 content."));
     }
+    
+    [Test]
+    public async Task InvalidCsv()
+    {
+        var request = new RestRequest("/api/v1/parse-content", Method.Post);
+        request.AddJsonBody(new
+        {
+            Type = "csv",
+            Content = "IldpbGxpYW0gIkJpbGwiIEpvbmVzIiwiMzQ1IENhY3R1cyBEciIsIkNhbGlmb3JuaWEiCgo="
+        });
+
+        RestResponse response = await _restClient.ExecuteAsync(request);
+        var jsonResponse = JsonSerializer.Deserialize<JsonElement>(response.Content);
+        var detail = jsonResponse.GetProperty("detail").GetString();
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.That(detail, Is.EqualTo("Invalid CSV content."));
+    }
+    
+    [Test]
+    public async Task ShouldParseJson()
+    {
+        var request = new RestRequest("/api/v1/parse-content", Method.Post);
+        request.AddJsonBody(new
+        {
+            Type = "internal_json",
+            Content = "ewogICJuYW1lIjogIkpvaG4iLAogICJhZ2UiOiAzMCwKICAiY2l0eSI6ICJOZXcgWW9yayIsCiAgImlzU3R1ZGVudCI6IHRydWUKfQoK"
+        });
+
+        RestResponse response = await _restClient.ExecuteAsync(request);
+        var jsonResponse = JsonSerializer.Deserialize<JsonElement>(response.Content);
+        var city = jsonResponse.GetProperty("records")[0].GetProperty("city").GetString();
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(city, Does.Contain("New York"));
+    }
+    
+    [Test]
+    public async Task InvalidJson()
+    {
+        var request = new RestRequest("/api/v1/parse-content", Method.Post);
+        request.AddJsonBody(new
+        {
+            Type = "internal_json",
+            Content = "bmFtZSxhZ2UKSm9obiwzMA=="
+        });
+
+        RestResponse response = await _restClient.ExecuteAsync(request);
+        var jsonResponse = JsonSerializer.Deserialize<JsonElement>(response.Content);
+        var detail = jsonResponse.GetProperty("detail").GetString();
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.That(detail, Is.EqualTo("Invalid JSON content."));
+    }
+
 
     [OneTimeTearDown]
     public void TearDown()
